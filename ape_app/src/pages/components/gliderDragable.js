31a5cd042cf7d96c681cae63f31a4b46d37998be
Jared Zaragoza <95445763+K0i0s@users.noreply.github.com>
2023-10-13 16:34:48 -0700

    Update gliderDragable.js

--- ape_app/src/pages/components/gliderDragable.js
+++ ape_app/src/pages/components/gliderDragable.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 const GliderDragable = ({children}) => {
-    <div className='gliderDragable mx-[-8px] my-[-16px] px-0 py-4 select-none'>
-        {children}
-    </div>
+    return(
+        <div className='gliderDragable mx-[-8px] my-[-16px] px-0 py-4 select-none'>
+            {children}
+        </div>
+    )
 }
 
 export default GliderDragable;
