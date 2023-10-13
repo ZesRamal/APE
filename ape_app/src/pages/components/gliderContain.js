e279ba196c55161696ad191ab1adf7fc67b2b77e
Jared Zaragoza <95445763+K0i0s@users.noreply.github.com>
2023-10-13 16:34:00 -0700

    Update gliderContain.js

--- ape_app/src/pages/components/gliderContain.js
+++ ape_app/src/pages/components/gliderContain.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 const GliderContain = ({children}) => {
-    <div className='w-full my-0 mx-auto relative'>
-        {children}
-    </div>
+    return(
+        <div className='w-full my-0 mx-auto relative'>
+            {children}
+        </div>
+    );
 }
 
 export default GliderContain;
