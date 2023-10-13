0f0530266de85d20a7cb9cc856ed2d8b0b74089b
Jared Zaragoza <95445763+K0i0s@users.noreply.github.com>
2023-10-13 16:36:26 -0700

    Update pictureSpan.js

--- ape_app/src/pages/components/pictureSpan.js
+++ ape_app/src/pages/components/pictureSpan.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 const PictureSpan = ({children}) => {
-    <span className='pictureSpan m-0 p-0 absolute inset-0 box-border block overflow-hidden opacity-100 bg-transparent bg-left-top bg-repeat bg-scroll bg-none bg-auto bg-origin-padding bg-clip-border border-current border-solid border-0 border-image-none border-1 border-stretch border-100'>
-        {children}
-    </span>
+    return(
+        <span className='pictureSpan m-0 p-0 absolute inset-0 box-border block overflow-hidden opacity-100 bg-transparent bg-left-top bg-repeat bg-scroll bg-none bg-auto bg-origin-padding bg-clip-border border-current border-solid border-0 border-image-none border-1 border-stretch border-100'>
+            {children}
+        </span>
+    )
 }
 
 export default PictureSpan;
