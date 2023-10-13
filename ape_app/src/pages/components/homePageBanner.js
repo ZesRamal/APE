8f7fbe30eb7c45bee72d0346c75e8d45873b64a7
Jared Zaragoza <95445763+K0i0s@users.noreply.github.com>
2023-10-13 16:35:39 -0700

    Update homePageBanner.js

--- ape_app/src/pages/components/homePageBanner.js
+++ ape_app/src/pages/components/homePageBanner.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const HomePageBanner = ({children}) => {
-    <header className='homePageBanner flex flex-col items-center text-center bg-gradient-to-r from-sky-500 to-indigo-500 bg-[50%] bg-no-repeat bg-cover md:p-[7.6rem] p-[3.2rem]'>
-        {children}
-    </header>
+    return(
+        <header className='homePageBanner flex flex-col items-center text-center bg-gradient-to-r from-sky-500 to-indigo-500 bg-[50%] bg-no-repeat bg-cover md:p-[7.6rem] p-[3.2rem]'>
+            {children}
+        </header>
+    )
 }
 
 export default HomePageBanner;
