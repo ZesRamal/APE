bddbcb296f195b553976d3856c6eed804f0f59c9
Cesar Ramos <cesarfranramos@gmail.com>
2023-12-04 23:53:23 -0800

    front

---
+++ ape_app/src/pages/profile/index.jsx
@@ -0,0 +1,13 @@
+/*View Profile page*/
+import React from "react";
+import NavBar from "../navbar/navbar";
+
+const viewProfile = () => {
+    return(
+        <div className="box-border">
+            <NavBar/>
+        </div>
+    )
+}
+
+export default viewProfile;
